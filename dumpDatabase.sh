a04adfa36660ae87b44375e09cb02632b92e035f
Aakarsh94 <68553442+Aakarsh94@users.noreply.github.com>
2022-02-02 14:49:12 +0530

    final

--- dumpDatabase.sh
+++ dumpDatabase.sh
@@ -1,9 +1,3 @@
-#file-name: postgres-backup.sh
 #!/bin/bash
-date1=$(date +%Y%m%d-%H%M)
-#mkdir /backup/pg-backup
-PGPASSWORD="$PGPASSWORD" pg_dump -Fc -v -h $PGHOST -p $PGPORT -U $PGUSER --dbname=$PGDATABASE > /$fileShareName/$date1.dump
-#file_name="pg-backup-"$date1".tar.gz"
-#Compressing backup file for upload
-#tar -zcvf $file_name /backup
+PGPASSWORD="$PGSOURCEPASSWORD" pg_dump -b -v -h $PGSOURCEHOST -p $PGSOURCEPORT -U $PGSOURCEUSER -Ft -O --dbname=$PGSOURCEDATABASE > /$fileShareName/$PGSOURCEDATABASE.tar
 echo "Postgres-Backup-was-successful"
